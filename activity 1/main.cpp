d366e9b240be9709783f9a78e2a2a4c8ec7f281b
Miles MJ Jamon <pvzzombs@gmail.com>
2022-10-15 12:15:52 +0800

    Removed using namespace std on act. 1

--- activity 1/main.cpp
+++ activity 1/main.cpp
@@ -1,19 +1,16 @@
 #include <iostream>
+//using namespace std;
+int main(){
+  int a;
+  int b;
+  int sum;
 
-using namespace std;
-
-int main()
-{
-    int a;
-    int b;
-    int sum;
-
-    cout << "Get the sum of two numbers" << endl;
-    cout << "First Number: ";
-    cin >> a;
-    cout << "Second Number: ";
-    cin >> b;
-    sum = a + b;
-    cout << "The sum of two numbers: " << sum << endl;
-    return 0;
+  std::cout << "Get the sum of two numbers" << std::endl;
+  std::cout << "First Number: ";
+  std::cin >> a;
+  std::cout << "Second Number: ";
+  std::cin >> b;
+  sum = a + b;
+  std::cout << "The sum of two numbers: " << sum << std::endl;
+  return 0;
 }
